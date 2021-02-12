f700f18461855319b0a6d92ab75350db3c18e15e
Erik <erikjohansson890@gmail.com>
2021-02-12 14:30:03 +0100

    add person still not working

--- afrytest/src/components/HandleCompany.js
+++ afrytest/src/components/HandleCompany.js
@@ -8,8 +8,10 @@ import './styles/StylesHandleCompany.css'
 function HandleCompany() {
   const dispatch = useDispatch()
   const [companyEmployees, setCompanyEmployees] = useState([]);
+  const [showPersons, setShpwPersons] = useState(false);
+  const [selectedCompany, setSelectedCompany] = useState({});
   var activePersons = JSON.parse(localStorage.getItem("activePersons"))
-  console.log(activePersons)
+ // console.log(activePersons)
 
   var companyList = JSON.parse(localStorage.getItem("companyList"))
     console.log(companyList)
@@ -26,20 +28,38 @@ function handlePerson(person) {
 }
 
 function handleCompany(company) {
-   console.log(company.employees)
-   // setCompanyEmployees(company.employees)
-    // console.log(companyEmployees)
-
-    // for(let i=0; i < company.employees.length ; i++) {
-    //     console.log(company.employees[i])
-    // }
+ // console.log(company.employees)
+  setSelectedCompany(company)
+  localStorage.setItem('selectedCompany',JSON.stringify(company));
+    
 }
 
 
-function addEmployee() {
-    console.log("test")
-    
-    console.log(activePersons)
+function addEmployees() {
+   setShpwPersons(!showPersons)
+}
+
+function addPersonToCompany(person) {
+ 
+  var selectedCompanyEmpList = selectedCompany.employees
+  
+  selectedCompanyEmpList.push(person)
+ 
+  console.log(selectedCompanyEmpList)
+ 
+  //localStorage.setItem("addPerson", JSON.stringify(selectedCompanyEmpList));
+   //var testObject = selectedCompanyEmpList
+  // var companyList = JSON.parse(localStorage.getItem("selectedCompany"))
+  // console.log(companyList.employees)
+
+  
+  // localStorage.setItem('selectedCompany"', JSON.stringify(companyList.employees));
+  //companyList = testObject;
+  
+  //localStorage.setItem("companyList", JSON.stringify(companyList));
+
+
+
 }
 
 
@@ -48,25 +68,13 @@ function addEmployee() {
 return (
     <div className="handle-company-container">
      <span>EDIT COMPANY</span>
-        <div className="person-list-container">
-            { activePersons === null ? <span>no persons</span> : 
-                <ul>
-                    { activePersons.map((person, index) =>{
-                        return <li key={index} onClick={() => handlePerson(person)}>
-                                  <span>Name: {person.name}</span>
-                                  <span>Lastname: {person.lastName}</span>
-                                </li>
-                    })}
-                </ul>
-            }
-           </div>
-           <div className="company-list-container">
+        <div className="company-list-container">
             { companyList === null ? <span>no companies</span> : 
                 <ul>
                     { companyList.map(function (company, index) {
                         return (<div className="li-container" key={index}>
                                 <li  onClick={() => handleCompany(company)}>
-                                  <span>Company-Name:{company.companyName}</span>
+                                  <span>Company-Name: {company.companyName}</span>
                                     {/* <ul>
                                         { companyEmployees.map((emp,idx) => 
                                         <li key={idx}>{emp.name}</li>)}
@@ -74,13 +82,29 @@ return (
                                
                                     
                                </li> 
-                               <button onClick={() => addEmployee()}>Add Emplyee</button>
+                                <button onClick={() => addEmployees()}>Add Emplyees</button>
                                </div>)
-                               
+                      })}
+                </ul>
+            }
+        </div>
+            { showPersons ?
+        <div className="person-list-container">
+          
+            { activePersons === null ? <span>no persons</span> : 
+                <ul>
+                    { activePersons.map((person, index) =>{
+                        return <li key={index} onClick={() => handlePerson(person)}>
+                                  <span>Name: {person.name}</span>
+                                  <span>Lastname: {person.lastName}</span>
+                                  <button onClick={() => addPersonToCompany(person)}>Add to Company</button>
+                                </li>
                     })}
                 </ul>
             }
-           </div>
+           </div>  
+          : null } 
+
     </div>
   );
 }
