3614f59c78874e785d2e11fc6a53736030bcaa43
Erik <erikjohansson890@gmail.com>
2021-02-18 13:49:57 +0100

    clear input fields

--- afrytest/src/components/CreatePerson.js
+++ afrytest/src/components/CreatePerson.js
@@ -1,72 +1,74 @@
-import React, { useState, useEffect}  from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 //CSS
 import './styles/stylesCreatePerson.css'
 
 function CreatePerson() {
-    
+
     const [person, setPerson] = useState({
         name: "",
         lastName: "",
     });
-    
-        useEffect( () => {
-                var existingEntries = JSON.parse(localStorage.getItem("activePersons"))
-              //  console.log(existingEntries)
 
-        },[]) //useEffect
+    useEffect(() => {
+        var existingEntries = JSON.parse(localStorage.getItem("activePersons"))
+        //  console.log(existingEntries)
 
-        const handleInput = (event) => {
-              setPerson((prevProps) => ({
-                        ...prevProps,
-                        [event.target.name]: event.target.value
-                }));
-        };
-    
-        const handleSubmit = (event) => {
-                event.preventDefault();
-            
-                var personList = JSON.parse(localStorage.getItem("activePersons"));
-                if(personList == null) personList = [];
-                localStorage.setItem("entry", JSON.stringify(person));
-                personList.push(person);
-                localStorage.setItem("activePersons", JSON.stringify(personList));
+    }, []) //useEffect
 
-                document.getElementsByTagName("input")[0].value = ""
-                document.getElementsByTagName("input")[1].value = ""
+    const handleInput = (event) => {
+        setPerson((prevProps) => ({
+            ...prevProps,
+            [event.target.name]: event.target.value
+        }));
     };
-    
-return (
-    <div className="create-person-container">
-                <span className="title">Create A Person</span>
-                <form id="myForm" onSubmit={handleSubmit}>
-                    <div className="form-control">
-                        <label>First Name :</label>
-                            <input
-                                type="text"
-                                name="name"
-                                value={person.name}
-                                placeholder="First Name"
-                                onChange={handleInput}
-                            />
-                    </div>
-                    <div className="form-control">
-                        <label>Last Name :</label>
-                            <input
-                                type="text"
-                                name="lastName"
-                                value={person.LastName}
-                                placeholder="Last Name"
-                                onChange={handleInput}
-                            />
-                    </div>
-                   <div className="form-btn">
-                        <button className="btn-submit" type="submit">Create Person</button>
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        var personList = JSON.parse(localStorage.getItem("activePersons"));
+        if (personList == null) personList = [];
+        localStorage.setItem("entry", JSON.stringify(person));
+        personList.push(person);
+        localStorage.setItem("activePersons", JSON.stringify(personList));
+
+        setPerson((prevProps) => ({
+            ...prevProps,
+            [event.target.name]: ""
+        }));
+    };
+
+    return (
+        <div className="create-person-container">
+            <span className="title">Create A Person</span>
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label>First Name :</label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={person.name}
+                        placeholder="First Name"
+                        onChange={handleInput}
+                    />
+                </div>
+                <div className="form-control">
+                    <label>Last Name :</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        value={person.LastName}
+                        placeholder="Last Name"
+                        onChange={handleInput}
+                    />
+                </div>
+                <div className="form-btn">
+                    <button className="btn-submit" type="submit">Create Person</button>
                 </div>
             </form>
-    </div>
-);
+        </div>
+    );
 }
 
 export default CreatePerson;
